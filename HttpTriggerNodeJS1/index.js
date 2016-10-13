72bea651bbbb24c00bfeb92643da01cb227f24a7
Mitsunori Yorifuji <yorifuji.github@gmail.com>
2016-10-14 00:15:08 +0900

    validate x-line-signature

--- HttpTriggerNodeJS1/index.js
+++ HttpTriggerNodeJS1/index.js
@@ -1,7 +1,21 @@
+
+var crypto = require("crypto");
+
+function validate_signature(signature, body)
+{
+    const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET;
+    return signature == crypto.createHmac('sha256', LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
+}
+
 module.exports = function(context, req) {
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
-    context.bindings.outputQueueItem = req.body;
+    if (validate_signature(req.headers['x-line-signature'], req.body)) {
+	context.bindings.outputQueueItem = req.body;
+    }
+    else {
+	context.log('fail to validate signature');
+    }
     
     context.res = { body : "" };
     context.done();
